c6a3ded0190440fb45c083c9d5bcb6cf6efd472d
Apple Juice <mallari23adrian@gmail.com>
2022-11-06 23:50:11 +0800

    Added Bottom Nav

--- app/src/main/java/com/example/pintas/Login_Activity.kt
+++ app/src/main/java/com/example/pintas/Login_Activity.kt
@@ -4,14 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login_Activity : AppCompatActivity() {
 
-
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        auth = Firebase.auth
 
         var login_button = findViewById<Button>(R.id.login_button)
         val home = Intent(this, MainActivity::class.java)
@@ -20,4 +25,11 @@ class Login_Activity : AppCompatActivity() {
             startActivity(home)
         }
     }
+
+    public override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+    }
+
 }
